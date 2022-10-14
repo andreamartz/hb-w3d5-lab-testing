2833ab1bb6ecbff1a628b899591d28270b0c0966
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-10-14 12:12:07 -0500

    Add doctests
    Co-authored-by: andreamartz 14165788+andreamartz@users.noreply.github.com
    Co-authored-by: lani lanijeylv@gmail.com

--- party.py
+++ party.py
@@ -7,16 +7,42 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+    
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+    >>> is_mel('Judith Butler', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Mel Melitpolski', 'mell@ubermelon.com')
+    True
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
-
+    
     Return most and least common treat types in tuple of format (most, least).
-    """
+
+    >>> most_and_least_common_type([
+            {'type': 'dessert'},
+            {'type': 'dessert'},
+            {'type': 'appetizer'},
+            {'type': 'dessert'},
+            {'type': 'appetizer'},
+            {'type': 'drink'}
+        ])
+    ('dessert', 'drink')
+
+    >>> most_and_least_common_type([])
+    (most_type, least_common_type)
+
+    >>> most_and_least_common_type([{'type': 'dessert'},{'type': 'dessert'},{'type': 'appetizer'},{'type': 'appetizer'}])
+    ('dessert', 'dessert')
+    """ 
 
     types = {}
 
